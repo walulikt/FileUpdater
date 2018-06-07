85ac81523568b4af8cf4f4b049fb52445c562026
walulikt <walulik.t@wp.pl>
2018-06-07 13:13:11 +0200

    runner exception handler

--- src/main/java/FileUpdaterRunner.java
+++ src/main/java/FileUpdaterRunner.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileUpdaterRunner {
@@ -6,29 +5,73 @@ public class FileUpdaterRunner {
 	public static void main(String[] args) {
 		FileUpdaterApi fUApi = new FileUpdaterApi();
 		StringToByteConverter conv = new StringToByteConverter();
-		
 		Scanner sc = new Scanner (System.in);
 		boolean endRunner = false;
+		
 		System.out.println("Witam programisto. Wlasnie uruchomiles program, który zmieni Twoj swiat. \n "
 				+ "Teraz bêdziesz mogl podac nazwe folderu w ktorym sa pliki o rozszerzeniu, które Cie interesuje "
 				+ "a na koniec zamienic wskazany ciag bajtów w tych plikach na taki jaki wskazesz.");
 		
 		while(!endRunner) {
+			boolean arrayConverted=false;
 			System.out.println("Podaj nazwê szukanego folderu: ");
 			String directoryName = sc.nextLine();
+			while(directoryName.isEmpty()) {
+				System.out.println("Musisz wprowadzic nazwê folderu: ");
+				directoryName = sc.nextLine();
+			} 
 				
 			System.out.println("Podaj format plikow w ktorych chcesz dokonac zmiany:");
 			String fileType = sc.nextLine();
+			while(fileType.isEmpty()||fileType.charAt(0)!='.') {
+				System.out.println("Musisz wprowadzic typ szukanego pliku (np.'.bit'): ");
+				fileType = sc.nextLine();
+			} 
 				
-			System.out.println("Podaj ciag bajtow, ktore chcesz zmienic oddzielone przecinkiem (przyk³ad: 2,50,60,87):");
-			String byteArray1 = sc.next();
-			byte [] userArray1 =conv.convert(byteArray1);
-			sc.nextLine();
+			System.out.println("Podaj ciag bajtow, ktore chcesz zmienic w pliku (przyk³ad: 2 50 60 87):");
+			String byteArray1 = sc.nextLine();
+			byte [] userArray1 = null;
+			while(byteArray1.isEmpty()) {
+				System.out.println("Ci¹g bajtów nie mo¿e byæ pusty. Wprowadz ci¹g bajtów");
+				byteArray1 = sc.nextLine();
+			}
+			while(!arrayConverted) {
+				try {
+					userArray1 =conv.convert(byteArray1);
+					arrayConverted=true;
+				} catch (NumberFormatException e) {
+					System.out.println(e.toString()+'\n'+"Podany ci¹g zawiera b³êdny format lub wychodzi poza zakres wartoœci bajta. Wprowadz ponownie: ");
+					byteArray1 = sc.nextLine();
+					while(byteArray1.isEmpty()) {
+						System.out.println("Ci¹g bajtów nie mo¿e byæ pusty. Wprowadz ci¹g bajtów");
+						byteArray1 = sc.nextLine();
+					}
+					arrayConverted=false;
+				}
+			}
 		
-			System.out.println("Podaj ciag bajtow, ktore chcesz wprowadzic:oddzielone przecinkiem (przyk³ad: 2,50,60,87)");
-			String byteArray2 = sc.next();
-			byte [] userArray2 =conv.convert(byteArray2);
-			sc.nextLine();
+			System.out.println("Podaj ciag bajtow, ktore chcesz wprowadzic do pliku (przyk³ad: 2 50 60 87):");
+			String byteArray2 = sc.nextLine();
+			byte [] userArray2 =null;
+			while(byteArray1.isEmpty()) {
+				System.out.println("Ci¹g bajtów nie mo¿e byæ pusty. Wprowadz ci¹g bajtów");
+				byteArray1 = sc.nextLine();
+			}
+			arrayConverted=false;
+			while(!arrayConverted) {
+				try {
+					userArray2 =conv.convert(byteArray2);
+					arrayConverted=true;
+				} catch (NumberFormatException e) {
+					System.out.println(e.toString()+'\n'+"Podany ci¹g zawiera b³êdny format lub wychodzi poza zakres wartoœci bajta. Wprowadz ponownie: ");
+					byteArray2 = sc.nextLine();
+					while(byteArray1.isEmpty()) {
+						System.out.println("Ci¹g bajtów nie mo¿e byæ pusty. Wprowadz ci¹g bajtów");
+						byteArray1 = sc.nextLine();
+					}
+					arrayConverted=false;
+				}
+			}
 		
 			fUApi.findDirectoryByName(directoryName);
 			if (fUApi.findAllFilesByType(fileType)) {
--- src/main/java/StringToByteConverter.java
+++ src/main/java/StringToByteConverter.java
@@ -1,26 +1,18 @@
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 
 public class StringToByteConverter {
-	private byte[] userArray1=null;
+	private byte[] byteArray=null;
 	
 	public byte[] getUserArray1() {
-		return userArray1;
+		return byteArray;
 	}
 
-	public byte[] convert (String input){
-		ArrayList<Byte> bList = new ArrayList<>();
-		String[] inputArray = input.split(",");
-		byte[] byteArray = new byte[inputArray.length];
+	public byte[] convert (String input) {
+		String[] inputArray = input.split(" ");
+		byteArray = new byte[inputArray.length];
 		for (int i=0; i<inputArray.length;i++) {
-			bList.add(i,Byte.valueOf(inputArray[i]));
-		}
-		userArray1 = new byte[bList.size()];
-		for(int i=0; i<bList.size(); i++) {
-			userArray1[i] = bList.get(i);
-		}
-		return this.userArray1;
+			byteArray[i]=Byte.parseByte(inputArray[i]);
+		}		
+		return byteArray;
 	}
 	
 }
